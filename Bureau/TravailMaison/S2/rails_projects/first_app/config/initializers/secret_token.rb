138b329eb502e7f73df592286bd692f030b9db57
Caminin <charlot.rodolphe@gmail.com>
2016-02-03 10:37:16 +0100

    j enleve les betises de ascri

--- Bureau/TravailMaison/S2/rails_projects/first_app/config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-FirstApp::Application.config.secret_token = '358ae2ce9a582cf24d1d3040346794f383c5f934757645b48feb5178521515e0eabbccea9eb1f29719371715e1dda204463130fa760d32d963d63c77e2716ba3'
