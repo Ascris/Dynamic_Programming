b925c9360fbce7e7baf8d739aae976323c5c97b2
Caminin <charlot.rodolphe@gmail.com>
2016-02-04 16:15:10 +0100

    add my loadFile function in main.c

--- main.c
+++ main.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
+#include <string.h>
 #include <stdlib.h>
+/*
 #include <fstream>
 #include <sstream>
 #include <vector>
@@ -89,4 +91,149 @@ int main(int argc, char* argv[])
 	} else {
 	    cerr << "Impossible d'ouvrir le fichier";
 	}
-}//fin main
+}//fin main*/
+
+struct item{
+    int id;
+    int a;
+    int c;
+};
+typedef struct item item;
+
+
+
+int get_file_size(char *file_name)
+{
+   FILE *fp;
+   int file_size;
+   file_size = 0;
+   if ((fp = fopen(file_name, "rb" )) == NULL) {
+      fprintf(stderr, "Cannot open %s.\n", file_name);
+      return(file_size);
+   }
+   char ligne[30];
+   while(fgets(ligne, 30,fp) != NULL)
+    {
+	    file_size++;
+    }
+   fclose(fp);
+   return(file_size);
+}
+
+char** str_split(char* a_str, const char a_delim)
+{
+    char** result    = 0;
+    size_t count     = 0;
+    char* tmp        = a_str;
+    char* last_comma = 0;
+    char delim[2];
+    delim[0] = a_delim;
+    delim[1] = 0;
+
+    /* Count how many elements will be extracted. */
+    while (*tmp)
+    {
+        if (a_delim == *tmp)
+        {
+            count++;
+            last_comma = tmp;
+        }
+        tmp++;
+    }
+
+    /* Add space for trailing token. */
+    count += last_comma < (a_str + strlen(a_str) - 1);
+
+    /* Add space for terminating null string so caller
+       knows where the list of returned strings ends. */
+    count++;
+
+    result =(char**) malloc(sizeof(char*) * count);
+
+    if (result)
+    {
+        size_t idx  = 0;
+        char* token = strtok(a_str, delim);
+
+        while (token)
+        {
+            *(result + idx++) = strdup(token);
+            token = strtok(0, delim);
+        }
+        *(result + idx) = 0;
+    }
+
+    return result;
+}
+
+
+void loadFile(char* filename, int *n, int *b, item **it)
+{
+    
+    
+    int size=get_file_size(filename);	
+	FILE *fichier;
+    fichier=fopen(filename,"r");
+	
+	if (fichier != NULL)
+	{		
+		char ** text=(char**)malloc(sizeof(char*)*size);
+		char ligne[30];
+		int num_ligne;
+		for(num_ligne=0; fgets(ligne, 30,fichier) != NULL; ++num_ligne)
+		{
+			text[num_ligne]=(char*)malloc(sizeof(char)*(strlen(ligne)+1));
+			strcpy(text[num_ligne],ligne);
+		}
+		
+		char **ligne1=str_split(text[0],' ');
+        
+		int nb_noeuds=atoi(ligne1[0]);
+        int capacity=atoi(ligne1[1]);
+        
+        printf("%d %d",nb_noeuds,capacity);
+        
+        *it=(item*)malloc(sizeof(item)*nb_noeuds);
+		
+		free(ligne1);
+		
+		int i;
+		for(i=1;i<nb_noeuds+1;i++)
+		{
+			char **ligne1=str_split(text[i],' ');
+            
+            unsigned id=(unsigned)atoi(ligne1[0]);
+			int size=atoi(ligne1[1]);
+			int cost=atoi(ligne1[2]);
+			
+            (*it)[i-1].id=id;
+            (*it)[i-1].a=size;
+            (*it)[i-1].c=cost;
+			free(ligne1);
+		}
+		
+		for (i= 0; i < size; ++i){
+		    free(text[i]);
+		}
+		free(text);
+		fclose(fichier);
+        
+        *n=nb_noeuds;
+        *b=capacity;
+	}
+	else
+	{
+	  // On affiche un message d'erreur si on veut
+	  fprintf(stderr, "%s\n","Impossible d'ouvrir le fichier \n");
+	}
+}
+
+int main()
+{
+    int n,b;
+    item *it;
+    char c[]="./txtfiles/GARNIER_Antoine.txt";
+    
+    loadFile(c,&n,&b,&it);
+}
+
